a625b57239e8e1ab2163aa1798595786c39cea73
Michael Schiumo <mschiumo18@gmail.com>
2019-04-24 16:28:39 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
     if @song.save
-       redirect_to song_path(@song)
+       redirect_to @song
     else
       render :new
     end
@@ -29,7 +29,7 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
     @song.destroy
 
-    redirect_to song_url
+    redirect_to songs_url
   end
 
   def update
@@ -40,16 +40,10 @@ class SongsController < ApplicationController
       render :edit
     end
   end
-
-  end
-
-
+end
 
   private
 
   def song_params
     params.require(:song).permit(:title, :released, :artist_name, :genre, :release_year)
   end
-
-
-end
